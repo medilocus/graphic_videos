29c7709d15d104bae22acc28d637a519d92a3eef
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-01 22:23:47 -0800

    Base export function.

---
+++ graphics/export.py
@@ -0,0 +1,30 @@
+#
+#  Graphic Videos
+#  An API for creating graphic videos in Python.
+#  Copyright Medilocus 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import pygame
+import cv2
+from . import options
+
+
+def export(resolution, fps, scenes):
+    """
+    :param resolution: Resolution of video.
+    :param fps: FPS of video.
+    :param scenes: List of scenes to export in order.
+    """
