1a2a99af78f9e86fca14b68050f68f345db79ed3
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-02 10:40:59 -0800

    Rectangle border.

--- graphics/elements/simple.py
+++ graphics/elements/simple.py
@@ -28,22 +28,31 @@ class Rect:
 
     loc: VectorProp
     size: VectorProp
+    border: IntProp
     color: VectorProp
+    border_color: VectorProp
     antialias: BoolProp
 
-    def __init__(self, loc: Tuple[int], size: Tuple[int], color: Tuple[int], antialias: bool = True) -> None:
+    def __init__(self, loc: Tuple[int], size: Tuple[int], border: int, color: Tuple[int], border_color: Tuple[int], antialias: bool = True) -> None:
         """
         Initializes rectangle.
         :param loc: Top left corner location (pixels) of rectangle.
         :param size: Size (x, y) pixels of rectangle.
+        :param border: Border width (pixels) of rectangle. Set to 0 to disable border.
         :param color: Color (rgba, 0 to 255) of rectangle. The ALPHA will be set to 255 if no alpha is given.
+        :param border_color: Border color of rectangle.
         :param antialias: Whether to perform simple antialiasing when rendering.
         """
         if len(color) == 3:
             color = (*color, 255)
+        if len(border_color) == 3:
+            border_color = (*border_color, 255)
+
         self.loc = VectorProp(2, IntProp, loc)
         self.size = VectorProp(2, IntProp, size)
+        self.border = IntProp(border)
         self.color = VectorProp(4, IntProp, color)
+        self.border_color = VectorProp(4, IntProp, border_color)
         self.antialias = BoolProp(antialias)
 
     def render(self, res: Tuple[int], frame: int, transp: bool = True) -> pygame.Surface:
@@ -60,12 +69,16 @@ class Rect:
 
         loc = self.loc.get_value(frame)
         size = self.size.get_value(frame)
+        border = self.border.get_value(frame)
         color = self.color.get_value(frame)
+        border_color = self.border_color.get_value(frame)
         antialias = self.antialias.get_value(frame)
 
-        if antialias:
+        if antialias:  # todo improve
             pygame.draw.rect(surface, (*color[:3], int(color[3]/2)), (loc[0]-1, loc[1]-1, size[0]+2, size[1]+2))
         pygame.draw.rect(surface, color, (*loc, *size))
+        if border > 0:
+            pygame.draw.rect(surface, border_color, (*loc, *size), border)
 
         return surface
 
