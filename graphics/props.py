7f8f56ba08b6b85d07c47ac34ff21bd1eb5a5933
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-01 20:59:47 -0800

    Keyframe class.

--- graphics/props.py
+++ graphics/props.py
@@ -17,6 +17,21 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+from .options import *
+
+
+class Keyframe:
+    def __init__(self, frame, value, interpolation=DEFAULT_INTERPOLATION):
+        """
+        Initializes keyframe.
+        :param frame: Frame of the keyframe.
+        :param value: Value of the keyframe.
+        :param interpolation: Interpolation of this keyframe to the next (if it exists).
+        """
+        self.frame = frame
+        self.value = value
+        self.interpolation = interpolation
+
 
 class BoolProp:
     def __init__(self, default_val):
