3717d3fafb44b304a78a741ea590a219e91b8fab
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-05 12:09:53 -0800

    TitleHoriz effect.

--- graphics/elements/text.py
+++ graphics/elements/text.py
@@ -27,10 +27,39 @@ pygame.init()
 class TitleHoriz:
     """Two text horizontal title."""
 
+    loc: Tuple[int]
+    size: Tuple[int]
     text1: Text
     text2: Text
 
-    def __init__(self, frame_start: int, frame_len: int = 60, loc: Tuple[int] = (0, 0), size: Tuple[int] = (1920, 1080),
-            font: str = DEFAULT_FONT, text1: str = "Text 1", text2: str = "Text 2", text_col: Tuple[int] = (255, 255, 255)) -> None:
-        self.text1 = Text((0, 0), text_col, font, text1, 18)
-        self.text2 = Text((0, 0), text_col, font, text2, 18)
+    def __init__(self, frame_start: int, frame_len: int = 90, loc: Tuple[int] = (0, 0), size: Tuple[int] = (1920, 1080),
+            font: str = DEFAULT_FONT, font_size: int = 24, text1: str = "Text 1", text2: str = "Text 2",
+            text_col: Tuple[int] = (255, 255, 255)) -> None:
+
+        self.loc = loc
+        self.size = size
+        self.text1 = Text((0, 0), text_col, font, text1, font_size)
+        self.text2 = Text((0, 0), text_col, font, text2, font_size)
+
+        self.text1.loc.keyframe((-100, size[1]//3), frame_start, interp="LINEAR")
+        self.text1.loc.keyframe((size[0]//2-75, size[1]//3), frame_start+frame_len//3, interp="LINEAR")
+        self.text1.loc.keyframe((size[0]//2+75, size[1]//3), frame_start+frame_len//1.5, interp="LINEAR")
+        self.text1.loc.keyframe((size[0]+100, size[1]//3), frame_start+frame_len, interp="LINEAR")
+
+        self.text2.loc.keyframe((size[0]+100, size[1]//1.5), frame_start, interp="LINEAR")
+        self.text2.loc.keyframe((size[0]//2+75, size[1]//1.5), frame_start+frame_len//3, interp="LINEAR")
+        self.text2.loc.keyframe((size[0]//2-75, size[1]//1.5), frame_start+frame_len//1.5, interp="LINEAR")
+        self.text2.loc.keyframe((-100, size[1]//1.5), frame_start+frame_len, interp="LINEAR")
+
+    def render(self, res: Tuple[int], frame: int, transp: bool = True):
+        if transp:
+            surface = pygame.Surface(res, pygame.SRCALPHA)
+        else:
+            surface = pygame.Surface(res)
+
+        subsurf = pygame.Surface(self.size)
+        subsurf.blit(self.text1.render(res, frame), (0, 0))
+        subsurf.blit(self.text2.render(res, frame), (0, 0))
+
+        surface.blit(subsurf, self.loc)
+        return surface
