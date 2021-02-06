a7e18b37606c2d6aac4bed51db9ca0ca5c05db9f
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-05 23:18:10 -0800

    added documentation

--- graphics/elements/graphs.py
+++ graphics/elements/graphs.py
@@ -28,17 +28,26 @@ class BarGraphVert:
 
     loc: VectorProp
     size: VectorProp
-    choices: Tuple[StringProp]
+    categories: Tuple[StringProp]
     colors: Tuple[VectorProp]
     border: IntProp
     border_color: VectorProp
 
-    def __init__(self, loc: Tuple[int], size: Tuple[int], choices: Tuple[str], colors: Tuple[Tuple[int]] = None, border: int = 4, border_color: int = (0, 0, 0)):
-        if len(choices) != len(colors):
-            raise ValueError(f"The length of choices, {len(choices)} must be equal to the length of colors, {len(colors)}")
+    def __init__(self, loc: Tuple[int], size: Tuple[int], categories: Tuple[str], colors: Tuple[Tuple[int]] = None, border: int = 4, border_color: int = (0, 0, 0)):
+        """
+        Initializes vertical bar graph.
+        :param loc: Top left corner location (pixels) of vertical bar graph.
+        :param size: Size (x, y) pixels of vertical bar graph.
+        :param categories: The name of each category of vertical bar graph. This will be a column.
+        :param colors: Colors (rgba, 0 to 255) of each choice of vertical bar graph. The ALPHA will be set to 255 if no alpha is given.
+        :param border: Border width (pixels) of the axes of vertical bar graph.
+        :param border_color: Border color of vertical bar graph.
+        """
+        if len(categories) != len(colors):
+            raise ValueError(f"The length of categories, {len(categories)} must be equal to the length of colors, {len(colors)}")
         self.loc = VectorProp(2, IntProp, loc)
         self.size = VectorProp(2, IntProp, size)
-        self.choices = [StringProp(choices[i]) for i in range(len(choices))]
+        self.categories = [StringProp(categories[i]) for i in range(len(categories))]
         self.colors = [VectorProp(4, IntProp, (*colors[i], 255) if len(colors[i]) == 3 else colors[i]) for i in range(len(colors))]
         self.border = IntProp(border)
         self.border_color = VectorProp(2, IntProp, border_color)
