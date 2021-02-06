bb92c8a7b37be70e3da63c4d434a4d8e7e113a5a
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-05 23:24:36 -0800

    doc, new param, bug fix

--- graphics/elements/graphs.py
+++ graphics/elements/graphs.py
@@ -20,6 +20,7 @@
 from typing import Tuple
 import pygame
 from ..props import *
+import random
 pygame.init()
 
 
@@ -33,21 +34,25 @@ class BarGraphVert:
     border: IntProp
     border_color: VectorProp
 
-    def __init__(self, loc: Tuple[int], size: Tuple[int], categories: Tuple[str], colors: Tuple[Tuple[int]] = None, border: int = 4, border_color: int = (0, 0, 0)):
+    def __init__(self, loc: Tuple[int], size: Tuple[int], categories: Tuple[str], values: Tuple[int], colors: Tuple[Tuple[int]] = None, border: int = 4, border_color: int = (0, 0, 0)):
         """
         Initializes vertical bar graph.
         :param loc: Top left corner location (pixels) of vertical bar graph.
         :param size: Size (x, y) pixels of vertical bar graph.
         :param categories: The name of each category of vertical bar graph. This will be a column.
-        :param colors: Colors (rgba, 0 to 255) of each choice of vertical bar graph. The ALPHA will be set to 255 if no alpha is given.
+        :param colors: Colors (rgba, 0 to 255) of each choice of vertical bar graph. The ALPHA will be set to 255 if no alpha is given. None will make all colors random.
         :param border: Border width (pixels) of the axes of vertical bar graph.
         :param border_color: Border color of vertical bar graph.
         """
-        if len(categories) != len(colors):
-            raise ValueError(f"The length of categories, {len(categories)} must be equal to the length of colors, {len(colors)}")
+        if not (len(categories) == len(values) == len(colors)):
+            raise ValueError(f"The length of categories, {len(categories)} must be equal to the length of values, {len(values)}, as well as the length of colors, {len(colors)}")
         self.loc = VectorProp(2, IntProp, loc)
         self.size = VectorProp(2, IntProp, size)
         self.categories = [StringProp(categories[i]) for i in range(len(categories))]
-        self.colors = [VectorProp(4, IntProp, (*colors[i], 255) if len(colors[i]) == 3 else colors[i]) for i in range(len(colors))]
+        self.values = [IntProp(values[i]) for i in range(len(values))]
+        if colors is None:
+            self.colors = [VectorProp(4, IntProp, (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))]
+        else:
+            self.colors = [VectorProp(4, IntProp, (*colors[i], 255) if len(colors[i]) == 3 else colors[i]) for i in range(len(colors))]
         self.border = IntProp(border)
         self.border_color = VectorProp(2, IntProp, border_color)
