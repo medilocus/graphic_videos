33fc34da252f42237356a5817d2a2b4c91084c8a
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-07 12:36:34 -0800

    Utils file with cv2img2surf.

---
+++ graphics/utils.py
@@ -0,0 +1,32 @@
+#
+#  Graphic Videos
+#  An API for creating graphic videos in Python.
+#  Copyright Medilocus 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import pygame
+import cv2
+pygame.init()
+
+
+def cv2img2surf(img) -> pygame.Surface:
+    """
+    Converts cv2 image to pygame surface.
+    :param img: numpy.ndarray (cv2 image) to convert.
+    """
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    surf = pygame.image.frombuffer(img.tostring(), img.shape[1::-1], "RGB")
+    return surf
