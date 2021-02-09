6bac90591b1a5dfde7b2246fd5a8a6d06a0c8cf7
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-08 21:26:18 -0800

    Scene background color.

--- graphics/scene.py
+++ graphics/scene.py
@@ -19,6 +19,7 @@
 
 from typing import List, Tuple
 import pygame
+from .props import *
 from .elements import BaseElement
 pygame.init()
 
@@ -26,38 +27,43 @@ pygame.init()
 class Scene:
     """Scene object."""
 
-    _start: int
-    _end: int
-    _step: int
-    _pause: Tuple[int]
-    _elements: List[BaseElement]
+    start: int
+    end: int
+    step: int
+    pause: Tuple[int]
+    elements: List[BaseElement]
+    bg_col: VectorProp
 
-    def __init__(self, start: int, end: int, step: int = 1, before_pause: int = 30, after_pause: int = 30) -> None:
+    def __init__(self, start: int, end: int, step: int = 1, bg_col: Tuple[int] = (0, 0, 0),
+            before_pause: int = 30, after_pause: int = 30) -> None:
         """
         Initializes scene.
         :param start: Start frame of scene.
         :param end: End frame of scene.
         :param step: Frame step.
+        :param bg_col: Background color of scene.
         :param before_pause: Pause (frames) before the scene starts.
         :param after_pause: Pause (frames) after the scene starts.
         """
-        self._start = start
-        self._end = end
-        self._step = step
-        self._pause = (before_pause, after_pause)
-        self._elements = []
+        self.start = start
+        self.end = end
+        self.step = step
+        self.pause = (before_pause, after_pause)
+        self.elements = []
+        self.bg_col = VectorProp(3, IntProp, bg_col)
 
     def get_frames(self) -> List[int]:
         """
         Returns a list of all frames to render.
         """
-        return list(range(self._start, self._end+sum(self._pause), self._step))
+        return list(range(self.start, self.end+sum(self.pause), self.step))
 
     def add_element(self, element: BaseElement) -> None:
-        self._elements.append(element)
+        self.elements.append(element)
 
     def render(self, res, frame):
         surface = pygame.Surface(res)
-        for element in self._elements:
-            surface.blit(element.render(res, frame-self._pause[0]), (0, 0))
+        surface.fill(self.bg_col.get_value(frame))
+        for element in self.elements:
+            surface.blit(element.render(res, frame-self.pause[0]), (0, 0))
         return surface
