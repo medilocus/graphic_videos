473c6ae1ec553940a19bd4b15ec3f689a9b78564
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-11 15:28:29 -0800

    base slider

--- graphics/preview/elements.py
+++ graphics/preview/elements.py
@@ -1,5 +1,6 @@
 import pygame
 from ..options import get_font
+import numpy as np
 
 pygame.init()
 
@@ -83,3 +84,78 @@ class FrameText:
         if loc[0] <= mouse[0] <= loc[0]+size[0] and loc[1] <= mouse[1] <= loc[1]+size[1]:
             return True
         return False
+
+
+class Slider:
+    colors = {
+        "text": (255,) * 3,
+        "slider": (50,) * 3,
+        "cursor": (130,) * 3,
+        "arrows": (255,) * 3,
+        "boxes": (80,) * 3,
+        "highlighted_boxes": (120,) * 3,
+    }
+    tri_padx = 8
+    tri_pady = 5
+
+    def __init__(self, x, y, width, height=40, init_val=80, val_range=(1, 100), label="Slider", only_int=True):
+        self.x, self.y = x, y
+        self.width, self.height = width, height
+        self.font = pygame.font.SysFont("comicsans", height - 15)
+        self.label = label
+        self.range = val_range
+        self.value = init_val
+        self.dragging = False
+        self.to_int = only_int
+
+    def draw_arrows(self, window):
+        left = pygame.Surface((self.height,)*2)
+        right = pygame.Surface((self.height,)*2)
+        mx, my = pygame.mouse.get_pos()
+        colliding = self.x <= mx <= self.x + self.height and self.y <= my <= self.y + self.height
+        color = self.colors["highlighted_boxes"] if colliding else self.colors["boxes"]
+        left.fill(color)
+        colliding = self.x + self.width - self.height <= mx <= self.x + self.width and self.y <= my <= self.y + self.height
+        color = self.colors["highlighted_boxes"] if colliding else self.colors["boxes"]
+        right.fill(color)
+        l = self.tri_padx
+        r = self.height - self.tri_padx
+        t = self.tri_pady
+        m = self.height/2
+        b = self.height - self.tri_pady
+        pygame.draw.polygon(left, self.colors["arrows"], ((r, t), (l, m), (r, b)))
+        pygame.draw.polygon(right, self.colors["arrows"], ((l, t), (r, m), (l, b)))
+        window.blit(left, (self.x, self.y))
+        window.blit(right, (self.x + self.width - self.height, self.y))
+
+    def update(self, window, events):
+        self.draw(window)
+        mx, my = pygame.mouse.get_pos()
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.y <= my <= self.y + self.height:
+                    self.dragging = self.x + self.height <= mx <= self.x + self.width - self.height
+                    if self.x <= mx <= self.x + self.height:
+                        self.value = max(self.value - 1, self.range[0])
+                    elif self.x + self.width - self.height <= mx <= self.x + self.width:
+                        self.value = min(self.value + 1, self.range[1])
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.dragging = False
+
+        if self.dragging:
+            self.loc_to_value()
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.colors["slider"], (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(window, self.colors["cursor"], (self.value_to_loc() - self.height/2, self.y, self.height, self.height))
+        self.draw_arrows(window)
+        text = self.font.render(f"{self.label}: {self.value}", 1, self.colors["text"])
+        text_loc = (self.x + (self.width-text.get_width()) // 2, self.y + self.height + 5)
+        window.blit(text, text_loc)
+
+    def loc_to_value(self):
+        val = np.interp(pygame.mouse.get_pos()[0], (self.x + self.height*1.5, self.x + self.width - self.height*1.5), self.range)
+        self.value = int(val) if self.to_int else val
+
+    def value_to_loc(self):
+        return np.interp(self.value, self.range, (self.x + self.height*1.5, self.x + self.width - self.height*1.5))
