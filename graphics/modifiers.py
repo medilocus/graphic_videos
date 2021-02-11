5367783476b60e5c51baf4f0334b90c555c93aea
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-10 22:58:52 -0800

    init super

--- graphics/modifiers.py
+++ graphics/modifiers.py
@@ -161,6 +161,7 @@ class ModColorEnhance(Modifier):
         Initializes modifier.
         :param factor: Factor of color enhancement
         """
+        super().__init__()
         self.factor = FloatProp(factor)
 
     def modify(self, src: pygame.Surface, frame: int) -> pygame.Surface:
