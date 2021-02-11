0b492c951aabbfcd61c42909cbe8f403fd818bfb
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-10 22:56:17 -0800

    param documentation

--- graphics/modifiers.py
+++ graphics/modifiers.py
@@ -163,5 +163,5 @@ class ModColorEnhance(Modifier):
         """
         self.factor = FloatProp(factor)
 
-    def modify(self, src, frame):
+    def modify(self, src: pygame.Surface, frame: int):
         pass
