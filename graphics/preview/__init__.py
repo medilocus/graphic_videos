f88eabb3e106835a077eea93566fcff518129155
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-11 23:14:58 -0800

    bug fix

--- graphics/preview/__init__.py
+++ graphics/preview/__init__.py
@@ -27,13 +27,11 @@ pygame.init()
 FPS = 60
 
 
-def draw_frame(window, res, scenes, frame, width, height, bottom_bar_height):
-    surf = pygame.Surface(res, pygame.SRCALPHA)
+def draw_frame(window, res, scenes, frame, image):
+    image.fill((0, 0, 0, 0))
     for scene in scenes:
         if frame in scene.get_frames():
-            surf.blit(scene.render(res, frame), (0, 0))
-    window.blit(pygame.transform.scale(surf, (width, height-bottom_bar_height)), (0, 0))
-    pygame.display.update((0, 0, width, height-bottom_bar_height))
+            image.blit(scene.render(res, frame), (0, 0))
 
 
 def launch(resolution: Tuple[int], scenes: Tuple[Scene], resizable: bool = True) -> None:
@@ -48,6 +46,8 @@ def launch(resolution: Tuple[int], scenes: Tuple[Scene], resizable: bool = True)
     slider = Slider(min(num_frames), (min(num_frames), max(num_frames)))
     resized = playing = False
     bottom_bar_height = 25
+    image = pygame.Surface(resolution, pygame.SRCALPHA)
+    image.fill((0, 0, 0, 0))
 
     while True:
         clock.tick(FPS)
@@ -58,12 +58,14 @@ def launch(resolution: Tuple[int], scenes: Tuple[Scene], resizable: bool = True)
         if frame_text.draw(window, events, width, height, text_size, font):
             slider.set(int(frame_text.text))
             frame_text.text = str(slider.value)
-            draw_frame(window, resolution, scenes, slider.value, width, height, bottom_bar_height)
+            draw_frame(window, resolution, scenes, slider.value, image)
         if slider.update(window, events, width, height, width-text_size[0]-15, bottom_bar_height) or slider.dragging or playing:
             frame_text.text = str(slider.value)
-            draw_frame(window, resolution, scenes, slider.value, width, height, bottom_bar_height)
+            draw_frame(window, resolution, scenes, slider.value, image)
             if playing:
                 slider.set(slider.value + 1)
+
+        window.blit(pygame.transform.scale(image, (width, height-bottom_bar_height)), (0, 0))
 
         for event in events:
             if event.type == pygame.QUIT:
@@ -77,11 +79,11 @@ def launch(resolution: Tuple[int], scenes: Tuple[Scene], resizable: bool = True)
             if resizable:
                 if event.type == pygame.VIDEORESIZE:
                     resized = True
-                    draw_frame(window, resolution, scenes, slider.value, width, height, bottom_bar_height)
+                    draw_frame(window, resolution, scenes, slider.value, image)
                     width, height = event.size
 
                 elif event.type == pygame.ACTIVEEVENT and resized:
                     window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
                     resized = False
 
-        pygame.display.update((0, height-bottom_bar_height, width, bottom_bar_height))
+        pygame.display.update()
