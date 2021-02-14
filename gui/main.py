45677366c1394f59fd66a2f99f2132c33cc0db75
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-14 11:40:18 -0800

    GUI game loop.

---
+++ gui/main.py
@@ -0,0 +1,43 @@
+#
+#  Graphic Videos
+#  An API for creating graphic videos in Python.
+#  Copyright Medilocus 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import pygame
+from constants import *
+pygame.init()
+
+
+def main():
+    pygame.display.set_caption("Graphic Videos - GUI")
+    surface = pygame.display.set_mode((1280, 720))
+
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
+        pygame.display.update()
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        surface.fill(BLACK)
+
+
+main()
