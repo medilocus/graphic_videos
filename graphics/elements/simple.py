834f0b8b295c7006850bc84238575f12bbcb1521
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-16 09:04:09 -0800

    size doc

--- graphics/elements/simple.py
+++ graphics/elements/simple.py
@@ -143,6 +143,7 @@ class Ellipse(BaseElement):
     """Ellipse element."""
 
     loc: VectorProp
+    size: VectorProp
 
     def __init__(self, loc: Tuple[int] = (0, 0), size: Tuple[int] = (100, 200), color: Tuple[int] = (255, 255, 255),
             border: int = 0, border_color: Tuple[int] = (255, 255, 255)) -> None:
