e770085fefe5cc5afab677284ab7649686555ef5
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-19 22:16:34 -0800

    head length doc

--- graphics/elements/simple.py
+++ graphics/elements/simple.py
@@ -392,7 +392,7 @@ class Arrow(BaseElement):
         self.color = VectorProp(4, IntProp, color)
 
     @classmethod
-    def from_vector(cls, origin: Tuple[int], angle: float, magnitude: float, stem_width: int, head_width: int, head_length, color):
+    def from_vector(cls, origin: Tuple[int], angle: float, magnitude: float, stem_width: int, head_width: int, head_length: int, color):
         return cls()
 
     @staticmethod
