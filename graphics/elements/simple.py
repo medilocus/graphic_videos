14a96ec0ade1c4709c6316a401718320eea5fa28
ArjunSahlot <iarjun.sahlot@gmail.com>
2021-02-19 22:15:21 -0800

    angle doc

--- graphics/elements/simple.py
+++ graphics/elements/simple.py
@@ -392,7 +392,7 @@ class Arrow(BaseElement):
         self.color = VectorProp(4, IntProp, color)
 
     @classmethod
-    def from_vector(cls, origin: Tuple[int], angle, magnitude, stem_width, head_width, head_length, color):
+    def from_vector(cls, origin: Tuple[int], angle: float, magnitude, stem_width, head_width, head_length, color):
         return cls()
 
     @staticmethod
